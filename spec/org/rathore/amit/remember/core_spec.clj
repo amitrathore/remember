30dd17d7d8d80198d1a24a9de82eb50a0a2a9052
Amit Rathore <amitrathore@gmail.com>
2010-03-08 13:53:35 -0800

    added spec folder

---
+++ spec/org/rathore/amit/remember/core_spec.clj
@@ -0,0 +1,9 @@
+(ns org.rathore.amit.remember.core-spec
+  (:use clojure.test)
+  (:use org.rathore.amit.remember.core))
+
+(def user-id "583299457161")
+(def secret-key "2b+IIJh0aX3bQns0igv2ETvMlvVJTAR+ndaevk1g")
+(def access-key "0KQ1K5PE74WQJBKRGK82")
+
+

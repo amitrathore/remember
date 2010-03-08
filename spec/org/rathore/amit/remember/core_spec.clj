92a54aa23fb98ce203bddb808930464016fe052a
Amit Rathore <amitrathore@gmail.com>
2010-03-08 14:50:04 -0800

    removed stuff from core-spec

--- spec/org/rathore/amit/remember/core_spec.clj
+++ spec/org/rathore/amit/remember/core_spec.clj
@@ -2,10 +2,6 @@
   (:use clojure.test)
   (:use org.rathore.amit.remember.core))
 
-(def user-id "583299457161")
-(def secret-key "2b+IIJh0aX3bQns0igv2ETvMlvVJTAR+ndaevk1g")
-(def access-key "0KQ1K5PE74WQJBKRGK82")
 
-(def dev-bucket "runa-consumer-events-backup-development")
 
 

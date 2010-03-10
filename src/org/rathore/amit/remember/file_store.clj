208e22d4270fda5f54a6dc1a032518d89dfbf05c
Amit Rathore <amitrathore@gmail.com>
2010-03-09 18:51:09 -0800

    added file_store

--- src/org/rathore/amit/remember/file_store.clj
+++ src/org/rathore/amit/remember/file_store.clj
@@ -1,11 +1,9 @@
 (ns org.rathore.amit.remember.string-store
   (:use org.rathore.amit.remember.core)
-  (:use clojure.contrib.str-utils)
   (:import (org.jets3t.service.model S3Object)))
 
-(defn store-strings-in-bucket [bucket-name object-name data-strings]
+(defn store-file-in-bucket [bucket-name object-name filename]
   (let [bucket (get-bucket bucket-name)
-        data (str-join "\n" data-strings)
+        data (java.io.File. filename)
         s3-object (S3Object. object-name data)]
     (put-object bucket s3-object)))
-

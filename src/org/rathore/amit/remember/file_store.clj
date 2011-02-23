c303bf8a51a76c86b249c45d8bd79ca4e40ee366
karthik k <karthik@karthik.local>
2011-02-23 20:33:51 +0530

    set acl on the s3object to give read acess to all

--- src/org/rathore/amit/remember/file_store.clj
+++ src/org/rathore/amit/remember/file_store.clj
@@ -1,9 +1,12 @@
 (ns org.rathore.amit.remember.file-store
   (:use org.rathore.amit.remember.core)
-  (:import (org.jets3t.service.model S3Object)))
+  (:import (org.jets3t.service.model S3Object))
+  (:import (org.jets3t.service.acl AccessControlList)))
 
-(defn store-file-in-bucket [bucket-name object-name filename]
+(defn store-file-in-bucket [bucket-name  filename]
   (let [bucket (get-bucket bucket-name)
         data (java.io.File. filename)
-        s3-object (S3Object. bucket data)]
+        s3-object (S3Object. bucket data)
+	acl AccessControlList/REST_CANNED_PUBLIC_READ]
+    (.setAcl s3-object acl)
     (put-object bucket s3-object)))
